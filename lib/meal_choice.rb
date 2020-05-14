13372d722f631a00e7aee33bc8b8715f9c3a3571
Jonas Kavaliauskas <jonas.kavaliauskas@yale.edu>
2020-05-14 08:41:23 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,4 +2,6 @@
 # For output purposes, use "puts" instead of "print" or "p"
 
 def meal_choice(protein = tofu, veg1, veg2)
-  puts ""
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+  return "A plate of #{protein} with #{veg1} and #{veg2}."
+end  
